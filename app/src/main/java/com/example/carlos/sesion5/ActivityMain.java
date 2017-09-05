2487ee1ddb52d58370a04b3f706131247a53d9b5
carlosge10 <carlosge10@hotmail.com>
2017-09-04 22:49:26 -0500

    done

--- app/src/main/java/com/example/carlos/sesion5/ActivityMain.java
+++ app/src/main/java/com/example/carlos/sesion5/ActivityMain.java
@@ -1,16 +1,62 @@
 package com.example.carlos.sesion5;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 public class ActivityMain extends AppCompatActivity {
+
+    private Alumno a;
+
+    private Button save;
+    private Button clean;
+    private EditText nombre;
+    private EditText telefono;
+    private Spinner escolaridad;
+    private AutoCompleteTextView libro;
+    private CheckBox deporte;
+    private RadioButton femenino;
+    static final String NAME_FIELD = "nameField";
+    static final String PHONE_FIELD = "phoneField";
+    static final String ESCOLARIDAD_FIELD = "escolaridadField";
+    static final String GENERO_FIELD = "generoField";
+    static final String LIBRO_FIELD = "libroField";
+    static final String DEPORTE_FIELD = "deporteField";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        save = (Button) findViewById(R.id.activity_main_save);
+        clean = (Button) findViewById(R.id.buttonClean);
+        nombre = (EditText) findViewById(R.id.et_name);
+        telefono = (EditText) findViewById(R.id.et_phone);
+        escolaridad = (Spinner) findViewById(R.id.sp_escolaridad);
+        deporte = (CheckBox) findViewById(R.id.checkbox_deporte);
+        femenino = (RadioButton) findViewById(R.id.rb_femenino);
+
+        String [] arr = getResources().getStringArray(R.array.activity_main_libros);
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+        libro = (AutoCompleteTextView) findViewById(R.id.activity_main_libros);
+        libro.setAdapter(ad);
+
+        a=new Alumno();
+        a.setGenero("Femenino");
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,9 +68,39 @@ public class ActivityMain extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.activity_main_save:
-                Toast.makeText(this, "Guardar", Toast.LENGTH_LONG).show();
+                a.setNombre(nombre.getText().toString());
+                a.setTelefono(telefono.getText().toString());
+                a.setEscolaridad(escolaridad.getSelectedItem().toString());
+                a.setGenero( femenino.isChecked() ? "Femenino" : "Masculino" );
+                a.setLibro(libro.getText().toString());
+                a.setDeporte(deporte.isChecked());
+                Toast.makeText(this, "Guardar: \n" + a.toString(), Toast.LENGTH_LONG).show();
                 break;
         }
         return false;
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString(NAME_FIELD, a.getNombre() /*nombre.getText().toString()*/);
+        savedInstanceState.putString(PHONE_FIELD, a.getTelefono() /*telefono.getText().toString()*/);
+        savedInstanceState.putString(ESCOLARIDAD_FIELD, a.getEscolaridad() /*escolaridad.getSelectedItem().toString()*/);
+        savedInstanceState.putString(GENERO_FIELD, a.getGenero() /*((RadioButton)findViewById(R.id.rb_femenino)).isChecked() ?  "Femenino" : "Masculino"*/);
+        savedInstanceState.putString(LIBRO_FIELD, a.getLibro());
+        savedInstanceState.putBoolean(DEPORTE_FIELD, a.isDeporte());
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    public void limpiar(View view){
+        a = new Alumno();
+        nombre.setText("");
+        telefono.setText("");
+        escolaridad.setSelection(1);
+        ((RadioButton) findViewById(R.id.rb_femenino)).setChecked(true);
+        libro.setText("");
+        deporte.setChecked(false);
     }
 }
--- app/src/main/java/com/example/carlos/sesion5/Alumno.java
+++ app/src/main/java/com/example/carlos/sesion5/Alumno.java
@@ -5,6 +5,69 @@ package com.example.carlos.sesion5;
  */
 
 public class Alumno {
+    private String nombre;
     private String telefono;
+    private String escolaridad;
+    private String genero;
+    private String libro;
+    private boolean deporte;
 
+    public Alumno(){
+        deporte = false;
+        genero = "Femenino";
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEscolaridad() {
+        return escolaridad;
+    }
+
+    public void setEscolaridad(String escolaridad) {
+        this.escolaridad = escolaridad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getLibro() {
+        return libro;
+    }
+
+    public void setLibro(String libro) {
+        this.libro = libro;
+    }
+
+    public boolean isDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(boolean deporte) {
+        this.deporte = deporte;
+    }
+
+    public String toString(){
+        return "Nombre: " + getNombre() + "\n" + "Telefono: " + getTelefono() + "\n" + "Escolaridad: " + getEscolaridad() + "\n" +
+                "Genero: " + getGenero() + "\n" +"Libro: " + getLibro() + "\n" + "Practica Deporte: " + isDeporte();
+    }
 }
